6159a5ea8c8240c0268c0cff6580556fe16f4543
Giovanni León <giovannilc.dev@gmail.com>
2024-12-23 16:52:26 +0000

    serial-reader modificado, agregado lectura de byte y salida por ctrl+c

--- prueba-reader/prueba-reader.ino
+++ prueba-reader/prueba-reader.ino
@@ -1,7 +1,15 @@
-void setup(){}
+uint8_t count = 0;
+uint8_t sqr = 0;
+
+
+void setup(){
+}
 
 
 void loop(){
-  SerialUSB.println("Funciona adecuadamente");
+	count++;
+	sqr = pow(count,2);
+	Serial.write(count);
+	Serial.write(sqr);
   delay(3000);
 }
--- serial-reader.py
+++ serial-reader.py
@@ -9,8 +9,8 @@ class SerialReader:
     
     def __init__(self, port: str, baud_rate: int = 9600, timeout: float = 1.0):
         
+        self.exit = False
         self.__serial_buffer = Queue()
-        
         # Inicia el hilo de lectura de datos
         self.__characteristics_thread = threading.Thread(target=self.__read_arduino_serial, args=(port, baud_rate, timeout))
         self.__characteristics_thread.start()
@@ -23,18 +23,25 @@ class SerialReader:
                 time.sleep(2)  # Espera para asegurar la conexión
 
                 while True:
+                    if self.exit: break
                     if arduino.in_waiting > 0:  # Comprueba si hay datos disponibles
-                        data = arduino.readline().decode('utf-8').strip()
+                        data = int.from_bytes(arduino.read(1), byteorder='little')
                         self.__serial_buffer.put(data)
-
+                    
         except serial.SerialException as e:
             print(f"Error al acceder al puerto {port}: {e}")
-            self.__serial_buffer.put("ERROR")
-        except KeyboardInterrupt:
-            print("\nPrograma detenido por el usuario")
+            self.exit = True
+        except Exception as e:
+            print(f"Error inesperado: {e}")
+            self.exit = True
+            
 
     def get_queue(self):
         return self.__serial_buffer
+
+    def stop(self):
+        self.exit = True
+        self.__characteristics_thread.join()
 
 
 
@@ -43,10 +50,14 @@ class SerialReader:
 if __name__ == "__main__":
 
     port = "COM3"  
-    readerQueue = SerialReader(port).get_queue()
+    reader = SerialReader(port)
+    readerQueue = reader.get_queue()
 
-    
-    while True:
-        data = readerQueue.get()
-        if data == "ERROR": break 
-        print(data)
+    try:
+        while True:
+            data = readerQueue.get()
+            if reader.exit: break 
+            print(data)
+    except KeyboardInterrupt:
+        print("\nPrograma detenido por el usuario")
+        reader.stop()
