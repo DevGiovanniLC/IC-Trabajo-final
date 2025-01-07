ce0987e125f9f7d7a91a7800faa1cf36f314a92e
AdrianCardonaCandil <adriancardonacandil@gmail.com>
2025-01-07 20:26:29 +0000

    first commit, added paho library and mock initial mqtt client

---
+++ webapp/lib/mqttClient.js
@@ -0,0 +1,63 @@
+// Including the MQTT.js library
+import pahoMqtt from "../include/paho-mqtt"
+
+/* Definition of MQTTClient class:
+  1. Constructor: Initializes the MQTT client with the given options
+  2. Connect: Attempts to connect to the MQTT broker
+  3. OnConnect: If successfully connected, subscribes to the given topic
+  4. OnConnectionLost: Logs error message when connection is lost
+  5. Subscribe: Subscribes the client to the given topic
+  6. OnSubscribe: Logs a message when successfully subscribed
+  7. Publish: Publishes a message to the given topic
+  8. OnMessageArrived: Logs the message received from the broker
+*/
+class MQTTClient {
+    constructor(options) {
+        this.client = new pahoMqtt.Client(
+            options.host,
+            options.port,
+            options.clientId
+        )
+    }
+
+    connect() {
+        this.client.connect({
+            onSuccess: this.onConnect,
+            reconnect: true
+        })
+    }
+
+    onConnect() {
+        console.log("Connected to MQTT broker")
+        this.subscribe()
+    }
+
+    onConnectionLost(responseObject) {
+        if (responseObject.errorCode !== 0) {
+            console.log("onConnectionLost:" + responseObject.errorMessage);
+        }
+    }
+
+    subscribe() {
+        this.client.subscribe(options.topic, {
+            qos: 1, 
+            onSuccess: this.onSubscribe
+        })
+    }
+
+    onSubscribe() {
+        console.log("Subscribed to topic")
+    }
+
+    publish(message) {
+        this.client.publish(
+            options.topic,
+            message,
+            { qos: 1 }
+        )
+    }
+
+    onMessageArrived(message) {
+        console.log("onMessageArrived:" + message.payloadString);
+    }
+}
