24fa5c193235fab02fd11fca057336cd04f9c3b0
AdrianCardonaCandil <adriancardonacandil@gmail.com>
2025-01-08 00:46:06 +0000

    added node entry to git repo

---
+++ webapp/node.js
@@ -0,0 +1,11 @@
+const MQTTClient = require('./lib/mqttclient');
+
+const options = {
+    host: "localhost",
+    port: 8000,
+    path: "/mqtt",
+    clientId: "mqttjs_" + Math.random().toString(16).substr(2, 8),
+}
+
+const client = new MQTTClient(options);
+client.connect();
